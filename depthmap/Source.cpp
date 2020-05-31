a4be77fa90f252c8a7f2f71a17a802f305465bab
moldovanpaul75 <moldovan.paul75@gmail.com>
2020-05-31 22:46:29 +0300

    add project files

---
+++ depthmap/Source.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "common.h"
+#include <vector>
+#include <math.h>
+#include <iostream>
+#include <queue>
+#include <random>
+
+
+int main() {
+
+	printf("Hello world");
+
+	getchar();
+	return 0;
+}
